9a89a5b9ec81c5f4c1313e114c605b1944d47eb3
Animishka <povsedn@yandex.ru>
2021-05-02 17:53:12 +0300

    В этом файле описана форма для регистрации нового пользователя.

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from accounts.models import MyUser, UserProfile, UserAppointment
+from accounts.models import *
 
 
 class UserCreationForm(forms.ModelForm):
@@ -72,5 +72,5 @@ admin.site.register(MyUser, UserAdmin)
 # ... and, since we're not using Django's built-in permissions,
 # unregister the Group model from admin.
 admin.site.unregister(Group)
-admin.site.register(UserProfile)
+admin.site.register(UserPhoto)
 admin.site.register(UserAppointment)
