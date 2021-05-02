614af40496b19b719bb31e424f0ad378e44b9a82
Animishka <povsedn@yandex.ru>
2021-05-02 18:37:17 +0300

    В этом файле описаны все представления для работы приложения accounts. Комментарии внутри файла.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,13 +1,19 @@
+from PIL import Image
 from django.shortcuts import render, redirect  
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ValidationError
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 from django.http import HttpResponse
 from .models import *
 from .admin import UserCreationForm
 from .utils import AppointmentMixin
-from .forms import LogInForm, UserAppointmentForm
+import PIL
+from .forms import LogInForm, UserAppointmentForm, UserPhotoForm
+
 
 
 class RegisterView(View):
@@ -25,36 +31,35 @@ class RegisterView(View):
                       context={'form': bound_form})
 
 
-class AppointmentView(LoginRequiredMixin, View):
-    raise_exception = True
-
+class AppointmentView(View):
     def get(self, request):
         if request.user.is_authenticated:
             form = UserAppointmentForm()
-            id_user = request.user.id
-            user = MyUser.objects.get(id=id_user)
+            user = MyUser.objects.get(id=request.user.id)
             return render(request, 'accounts/appointment.html',
                           context={'form': form, 'user': user})
         return redirect('login_url')
-
 
     def post(self, request):
         bound_form = UserAppointmentForm(request.POST)
 
         if bound_form.is_valid():
-            bound_form.save()
-            return redirect('profile_url')
+            user = MyUser.objects.get(id=request.user.id)
+            appointment = user.userappointment_set.create(
+                specialist=bound_form.cleaned_data['specialist'],
+                date_appointment=bound_form.cleaned_data['date_appointment'],
+            )
+            return redirect('history_url')
         return render(request, 'accounts/appointment.html', context={'form': bound_form})
 
 
-
 def history_view(request):
-    print()
-    #print(dir(request))
-    print()
-    print(request.POST)
-    print()
-    pass
+    if request.user.is_authenticated:
+        user = MyUser.objects.get(id=request.user.id)
+        appointments = user.userappointment_set.all()
+        return render(request, 'accounts/history.html', context={
+            'appointments': appointments})
+    return redirect('login_url')
 
 
 class LoginView(View):
@@ -75,7 +80,9 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return redirect('profile_url')
-            return render(request, 'accounts/login.html', context={'form': bound_form})
+            else:
+                error = 'Such user does not exist'
+                return render(request, 'accounts/login.html', context={'form': bound_form, 'not_found': error})
         return render(request, 'accounts/login.html', context={'form': bound_form})
 
 
@@ -85,18 +92,34 @@ def logout_view(request):
         return redirect('home_url')
 
 
+def delete_confirmation_view(request):
+    if request.user.is_authenticated:
+        user = MyUser.objects.get(id=request.user.id)
+        return render(request, 'accounts/delete.html', context={'user': user})
+    return redirect('login_url')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = MyUser.objects.get(id=request.user.id).delete()
+        return redirect('home_url')
+    return redirect('login_url')
+
+
 def profile_view(request):
     if request.user.is_authenticated:
-        id_user = request.user.id
-        user_profile = MyUser.objects.get(id=id_user)
-        return render(request, 'accounts/profile.html', context={'user_profile': user_profile})
+        user_profile = MyUser.objects.get(id=request.user.id)
+        userphoto = user_profile.userphoto_set.all()
+        return render(request, 'accounts/profile.html', context={
+            'user_profile': user_profile,
+            'userphoto': userphoto,
+        })
     return redirect('login_url')
 
 
 class EditView(View):
     def get(self, request):
-        id = request.user.id
-        user = MyUser.objects.get(id=id)
+        user = MyUser.objects.get(id=request.user.id)
         bound_form = UserCreationForm(instance=user)
         return render(request, 'accounts/edit.html',
                       context={'form': bound_form, 'user': user})
@@ -107,7 +130,33 @@ class EditView(View):
         bound_form = UserCreationForm(request.POST, instance=user)
 
         if bound_form.is_valid():
-            bound_form.save()
+            data = bound_form.cleaned_data
+            data.save()
             return redirect('login_url')
         return render(request, 'accounts/edit.html',
                       context={'form': bound_form, 'user': user})
+    
+
+@receiver(pre_delete, sender=UserPhoto)
+def userphoto_delete(sender, instance, **kwargs):
+    instance.photo.delete(False)
+
+
+class UploadPhotoView(View):
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            form = UserPhotoForm()
+            user = MyUser.objects.get(id=request.user.id)
+            return render(request, 'accounts/userphoto.html',
+                          context={'form': form, 'user': user})
+        return redirect('login_url')
+
+    def post(self, request):
+        bound_form = UserPhotoForm(request.POST, request.FILES)
+        if bound_form.is_valid():
+            user = MyUser.objects.get(id=request.user.id)
+            user.userphoto_set.all().delete()
+            user.userphoto_set.create(photo=bound_form.cleaned_data['photo'])
+            return redirect('profile_url')
+        return render(request, 'accounts/userphoto.html', context={'form': bound_form})
