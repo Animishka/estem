c6fab26d452432682211ebcb0fb6544c6cf072a7
Animishka <povsedn@yandex.ru>
2021-05-02 17:56:15 +0300

    В этом файле описаны формы для создания записи к специалисту мед.учреждения, формы для входа в учетную запись и формы для обновления фотографии профиля пользователя.

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,58 +1,24 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import ModelForm, ImageField, widgets
+
 from .models import *
 
-"""
-def current_user_func(request):
-    id_user = request.user.id
-
-    return id_user
-"""
 
 class UserAppointmentForm(forms.Form):
-    """
-    CHOICE_SPECIALIST = (
-        ('СI', 'Сosmetologist Ivanov'),
-        ('DA', 'Dermatologist Andreev'),
-        ('DV', 'Dermatologist Vasilyev'),
-        ('CA', 'Сosmetologist Antonov'),
-    )
-
-    #specialist = forms.ChoiceField(choices=CHOICE_SPECIALIST)
-    #date_appointment = forms.DateTimeInput(widget=forms.DateTimeInput(attrs={'class': 'form-control'}))
-
-
-    
-    specialist = forms.ChoiceField(choices=CHOICE_SPECIALIST)
-    date_appointment = forms.DateTimeInput()
-
-    specialist = forms.CharField()
+    specialist = forms.ChoiceField(choices=UserAppointment.CHOICE_SPECIALIST)
     date_appointment = forms.DateTimeField()
 
-    class Meta:
-        model = UserAppointment
-        fields = ('specialist', 'date_appointment')
-    
-    def save(self, **kwargs):
-        #id_user = request.user.id
-        #specialist = self.cleaned_data.get('specialist')
-        #date_appointment = self.cleaned_data.get('date_appointment')
-        return self
-"""
-    specialist = forms.CharField(max_length=20)
-    date_appointment = forms.DateTimeField()
-    
-    #current_user_func(request)
-#работает save, но нужно понять как привязку к id делать
-    def save(self):
-        id_user = current_user_func()
-        current_user = MyUser.objects.get(id=id_user)
-        new_appointment = current_user.my_user_appointment.create(
-            specialist=self.cleaned_data['specialist'],
-            date_appointment=self.cleaned_data['date_appointment']
-        )
-        return new_appointment
+    specialist.widget.attrs.update({'class': 'form-control'})
+    date_appointment.widget.attrs.update({'class': 'form-control'})
 
+    def clean(self, *args, **kwargs):
+        try:
+            specialist = self.cleaned_data.get('specialist')
+            date_appointment = self.cleaned_data.get('date_appointment')
+            return super(UserAppointmentForm, self).clean(*args, **kwargs)
+        except:
+            raise ValidationError('You made a mistake in the entered data')
 
 
 class LogInForm(forms.Form):
@@ -60,20 +26,16 @@ class LogInForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     def clean(self, *args, **kwargs):
-        email = self.cleaned_data.get('email').strip()
-        password = self.cleaned_data.get('password').strip()
-
-        #if email and password:
-        #    user = MyUser.objects.filter(email=email)
-        #    if not user:
-        #        raise ValidationError('Such user does not exists')
-
-        return super(LogInForm, self).clean(*args, **kwargs)
-
-        #return self.clean(*args, **kwargs)
+        try:
+            email = self.cleaned_data.get('email').strip()
+            password = self.cleaned_data.get('password').strip()
+            return super(LogInForm, self).clean(*args, **kwargs)
+        except:
+            raise ValidationError('You made a mistake in the entered data')
 
 
+class UserPhotoForm(ModelForm):
 
-
-
-
+    class Meta:
+        model = UserPhoto
+        fields = ['photo']
